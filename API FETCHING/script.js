3d05fade8368fba37f98e742f7b5f8c6695b2dbf
PEANEETHA SARABU <104303294+praneethaSarabu@users.noreply.github.com>
2024-09-16 10:12:24 +0530

    Add files via upload

---
+++ API FETCHING/script.js
@@ -0,0 +1,14 @@
+const URL = "https://cat-fact.herokuapp.com/facts"
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
+
+const getFacts = async () => {
+    console.log("getting facts...");
+    let response = await fetch(URL);
+    console.log(response);
+    let data = await response.json();
+    factPara.innerText = data[0].text;
+};
+
+
+btn.addEventListener("click" , getFacts);
